d1b2f531bb85bb0aa4a9478dd034ed698be065f9
Aj <siyaatul001@gmail.com>
2022-11-17 14:13:50 +0530

    my first commit

---
+++ git practice/src/example1.java
@@ -0,0 +1,20 @@
+import java.util.TreeMap;
+
+public class example1 {
+
+	public static void main(String[] args) {
+        TreeMap tm = new TreeMap();
+        tm.put(1, "Atul");
+        tm.put(5, "Atul");
+        tm.put(4, "Atul");
+        tm.put(2, "Atul");
+        tm.put(10, "Atul");
+        
+        System.out.println(tm);
+        
+		
+		
+		
+	}
+
+}
